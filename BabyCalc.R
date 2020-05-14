aca9cf5519de2c5fac41967e9c282b9858f2ed26
Capanahan <35273436+CaptainLenahan@users.noreply.github.com>
2020-05-13 17:42:52 -0700

    Update BabyCalc.R
    This function has been updated to find the month old patients within the age column from Overlake and convert them into a decimal number for better visualizaiton work moving forward.

--- BabyCalc.R
+++ BabyCalc.R
@@ -6,5 +6,15 @@ Baby.Age.Calc <- function(AgeInput) {
   Age.Fraction <- format(round((AgeInput / 12), 2), nsmall = 2)
   as.double(Age.Fraction)
   return(Age.Fraction)
+Baby.Age.Calc <- function(DFInput) {
+  searchMO <- "m.o.|M.O."
+  ColName <- "Age"
+  babies <- str_detect(DFInput[[ColName]], searchMO)
+  DFInput[,ColName] <- deleteSrings( ColName, searchMO, DFInput)
+  numericRows <- as.numeric(unlist(DFInput[babies, ColName]))
+  DFInput[babies,ColName] <- format(round((numericRows / 12), 2), nsmall = 2)
+  DFInput <- DFInput[,ColName]
+  return(DFInput[,ColName]) #returns df with fraction of babies
 }
 
+# example call:  x <- Baby.Age.Calc(Overlake.Clean)
